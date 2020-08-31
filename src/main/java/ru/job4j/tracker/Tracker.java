a2d148e3a63f03c0d456b1cdc9ba0953fde4a2b6
OlegKolchin <commshepard@gmail.com>
2020-08-31 07:16:52 +0500

    6. Метод замены заявки. Tracker.replace [#307869]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -13,11 +13,15 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
+       int index = indexOf(id);
+       return index != -1 ? items[index] : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
@@ -43,6 +47,13 @@ public class Tracker {
         }
         return Arrays.copyOf(names, size);
 
+    }
+
+    public boolean replace (int id, Item item) {
+
+        items[indexOf(id)] = item;
+        item.setId(id);
+        return item.getId() == id ? true : false;
     }
 
  }
