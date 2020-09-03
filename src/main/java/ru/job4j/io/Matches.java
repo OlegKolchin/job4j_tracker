fe039dd29711b8506a99a53014221d27b5e7e6db
OlegKolchin <commshepard@gmail.com>
2020-09-03 20:50:30 +0500

    2. Scanner и чтение числа из консоли. [#307805]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -7,18 +7,30 @@ public class Matches {
         Scanner input = new Scanner(System.in);
         int select = 0;
         for (matches = 11; matches > 0; matches -= select) {
-            System.out.println("Осталось спичек - " + matches);
-            System.out.println("Введите число от одного до трех");
+            System.out.println("Осталось спичек - " + matches + System.lineSeparator());
+            System.out.println("Игрок 1, введите число от одного до трех");
             select = Integer.valueOf(input.nextLine());
-            if (select < 1 || select > 3) {
-                System.out.println("Вы ввели неверное число");
-                select = 0;
+            while (select < 1 || select > 3) {
+                System.out.println("Игрок 1, вы ввели неверное число, повторите ввод");
+                select = Integer.valueOf(input.nextLine());
             }
+            for (matches = matches - select; matches > 0; matches -= select) {
+                System.out.println("Осталось спичек - " + matches + System.lineSeparator());
+                System.out.println("Игрок 2, введите число от одного до трех");
+                select = Integer.valueOf(input.nextLine());
 
-        }
-        if (matches == 0) {
-            System.out.println("Cпички закончились. Вы победили");
+                while (select < 1 || select > 3 ) {
+                    System.out.println("Игрок 2, вы ввели неверное число, повторите ввод");
+                    select = Integer.valueOf(input.nextLine());
+                }
+                break;
+                }
+            }
+        System.out.println("Поздравляю, вы победили!");
+
         }
 
     }
-}
+
+
+
