9f0accac8f137270a44ee8b5beb17d6426ac6159
OlegKolchin <commshepard@gmail.com>
2020-09-12 23:25:59 +0500

    9.2. Тесты вывода на консоль [#307803]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -57,5 +57,56 @@ public class StartUITest {
         new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
+    @Test
+    public void whenShowItemsAction() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("item"));
+        Input in = new StubInput(
+                new String[] {"0", "1"}
+        );
+        UserAction[] actions = {
+                new ShowItemsAction(out),
+                new ExitAction(out)
+        };
+        assertThat(tracker.findAll()[0].getId() + " " + tracker.findAll()[0].getName(),
+                is (item.getId() + " " + item.getName()));
+    }
+
+    @Test
+    public void whenFindItemByIdAction() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("item"));
+        String id = Integer.toString(item.getId());
+        Input in = new StubInput(
+                new String[] {"0", id, "1"}
+        );
+        UserAction[] actions = {
+                new FindItemByIdAction(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenFindItemsByName() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("item"));
+        Input in = new StubInput(
+                new String[] {"0", item.getName(), "1"}
+        );
+        UserAction[] actions = {
+                new FindItemsByName(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(tracker.findByName("item")[0].getId() + " " + tracker.findByName("item")[0].getName(),
+                is(item.getId() + " " + item.getName()));
+    }
+
+
 
 }
