c29ab99a01e234dc19dd89cf683cfb9fdba06d24
OlegKolchin <commshepard@gmail.com>
2020-09-12 21:38:41 +0500

    9. Написать тесты на StartUI. [#307816]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,18 +7,52 @@ import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.CoreMatchers.*;
 public class StartUITest {
-
+    @Test
+    public void whenAddItem() {
+        Input in = new StubInput(
+                new String[] {"0", "Item name", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("Item name"));
+    }
 
 
     @Test
-    public void findItemById() {
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Replaced item"));
+        String replacedName = "New item name";
+        String id = Integer.toString(item.getId());
+        Input in = new StubInput(
+                new String[] {"0", id, replacedName, "1",}
+        );
+        UserAction[] actions = {
+                new EditAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
     }
 
     @Test
-    public void findItemsByName() {
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Deleted item"));
+        String id = Integer.toString(item.getId());
+        Input in = new StubInput(
+                new String[] {"0", id, "1"}
+        );
+        UserAction[] actions = {
+                new DeleteAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
 
-    @Test
-    public void init() {
-    }
 }
