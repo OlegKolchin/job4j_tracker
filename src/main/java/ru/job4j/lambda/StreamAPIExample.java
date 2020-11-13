855f8d7b41724bbacfe6b9cfe627d6597152131e
OlegKolchin <commshepard@gmail.com>
2020-11-13 07:50:11 +0500

    0.1. Отладка лямбда в IDEA [#307644]

--- src/main/java/ru/job4j/lambda/StreamAPIExample.java
+++ src/main/java/ru/job4j/lambda/StreamAPIExample.java
@@ -8,9 +8,9 @@ import java.util.stream.Collectors;
 public class StreamAPIExample {
     public static void main(String[] args) {
         List<Integer> list = List.of(1, 2, 3, 4, -6, -4);
-        List<Integer> positive = list.stream().filter(
-                integer -> integer > 0
-        ).collect(Collectors.toList());
+        List<Integer> positive = list.stream()
+                .filter(integer -> integer > 0)
+                .collect(Collectors.toList());
         System.out.println(positive);
     }
 }
