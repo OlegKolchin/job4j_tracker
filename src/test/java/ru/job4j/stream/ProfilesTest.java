b23649498a9522207ee7124b7877de87072acc1d
OlegKolchin <commshepard@gmail.com>
2020-11-14 22:04:37 +0500

    2. Список адресов [#307639]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ProfilesTest {
+    @Test
+    public void whenWorksCorrectly() {
+        Profiles profiles = new Profiles();
+        Address address1 = new Address("Moscow", "Tverskaya", 10, 15);
+        Address address2 = new Address("Tver", "Lenina", 15, 20);
+        Profile profile1 = new Profile(address1);
+        Profile profile2 = new Profile(address2);
+        List<Profile> list = List.of(profile1, profile2);
+        List<Address> rsl = List.of(address1, address2);
+        List <Address> expect = profiles.collect(list);
+        assertThat(rsl, is(expect));
+    }
+
+}
