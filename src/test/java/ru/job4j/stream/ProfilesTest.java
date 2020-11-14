f82e80fe563ee12d1eae3cc78aa6fc7b0126fb92
OlegKolchin <commshepard@gmail.com>
2020-11-15 01:10:41 +0500

    3. Уникальность элементов и сортировка. [#307640]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -22,4 +22,17 @@ public class ProfilesTest {
         assertThat(rsl, is(expect));
     }
 
+    @Test
+    public void whenUniqueSorted() {
+        Profiles profiles = new Profiles();
+        Address address1 = new Address("Moscow", "Tverskaya", 10, 15);
+        Address address2 = new Address("Tver", "Lenina", 15, 20);
+        Profile profile1 = new Profile(address1);
+        Profile profile2 = new Profile(address2);
+        Profile profile3 = new Profile(address1);
+        List<Profile> list = List.of(profile1, profile2, profile3);
+        List<Address> rsl = List.of(address1, address2);
+        List <Address> expect = profiles.collectUnique(list);
+        assertThat(rsl, is(expect));
+    }
 }
