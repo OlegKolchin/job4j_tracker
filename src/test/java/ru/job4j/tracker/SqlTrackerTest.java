745755c2d7e3f324d1f6f1cdcf1e33b91139f0f8
OlegKolchin <commshepard@gmail.com>
2021-06-27 22:38:39 +0500

    4.4. Liquibase. Интеграционные тесты. Proxy. [#95881]

---
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -0,0 +1,89 @@
+package ru.job4j.tracker;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ru.job4j.tracker.Item;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Properties;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class SqlTrackerTest {
+
+    private static Connection connection;
+
+    @BeforeClass
+    public static void initConnection() {
+        try (InputStream in = SqlTrackerTest.class.getClassLoader().
+                getResourceAsStream("test.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @AfterClass
+    public static void closeConnection() throws SQLException {
+        connection.close();
+    }
+
+    @After
+    public void wipeTable() throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("delete from items")) {
+            statement.execute();
+        }
+    }
+
+    @Test
+    public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
+        SqlTracker tracker = new SqlTracker();
+        tracker.init();
+        Item item = new Item("item");
+        tracker.add(item);
+        assertThat(tracker.findById(item.getId()), is(item));
+    }
+
+//    @Test
+//    public void whenReplace() {
+//        SqlTracker tracker = new SqlTracker();
+//        tracker.init();
+//        Item change = new Item("change");
+//        tracker.replace(2, change);
+//        assertThat(tracker.findById(2).getName(), is("change"));
+//    }
+//
+//    @Test
+//    public void whenDelete() {
+//        SqlTracker tracker = new SqlTracker();
+//        tracker.init();
+//        tracker.delete(1);
+//        assertThat(tracker.findById(1), is(nullValue()));
+//    }
+
+//    @Test
+//    public void whenFindAll() {
+//        SqlTracker tracker = new SqlTracker();
+//        tracker.init();
+//        List<Item> items = tracker.findAll();
+//        assertThat(tracker.findByName("change").get(0), is(items.get(0)));
+//    }
+
+}
